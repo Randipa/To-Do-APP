b587576001e1f4f630079fe60df117384f0246da
Saliya Pathum <96324718+Randipa@users.noreply.github.com>
2023-08-10 14:54:09 +0530

    Add files via upload

---
+++ src/components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Footer: React.FC = () => {
+  return (
+    <footer className="text-center py-6 font-medium">LOIT - FusionX </footer>
+  );
+};
+
+export default Footer;
